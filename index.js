326b347fd03f2102dee87bf905252b2e327d14bd
fjgl <fjluartes@gmail.com>
2018-10-16 09:56:50 +0800

    Add npm path for static files

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static('public'));
-
-app.get('/', function(req, res) {
-    res.sendFile('index.html');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket) {
     var con = 'user connected';
