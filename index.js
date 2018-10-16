22064ff0b570357757cbfbb790a8b3f513d87f90
fjgl <fjluartes@gmail.com>
2018-10-16 09:46:42 +0800

    Add public folder for static files

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile('index.html');
 });
 
 io.on('connection', function(socket) {
